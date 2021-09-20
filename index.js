32e3046922ea6474369cf634ae4ab356c55c36a7
krgarima <kumargarima59@gmail.com>
2021-09-20 19:47:42 +0530

    updated logic

--- index.js
+++ index.js
@@ -1,45 +1,45 @@
 const billAmount = document.querySelector("#bill-Amount");
 const cashReceived = document.querySelector("#cash-Received");
 const checkButton = document.querySelector("#checkButton");
-const  message= document.querySelector("#error-message");
-const  noOfNotes= document.querySelectorAll(".no-Of-Notes");
+const message = document.querySelector("#error-message");
+const noOfNotes = document.querySelectorAll(".no-Of-Notes");
 
 const availableNotes = [2000, 500, 100, 20, 10, 5, 1];
 
-checkButton.addEventListener("click", function validAmount()
-{
+checkButton.addEventListener("click", function validAmount() {
     hideMessage();
-    if(billAmount.value >= 0)
-    {
-        if(cashReceived.value >= billAmount.value)
-        {
-            const changeAmount = cashReceived.value - billAmount.value;
-            remainingAmount(changeAmount);
-        }
-        else
-        errorMessageDisplay("We have an opening for a dishwashing/cleaning man. Do you wish to apply to cover the remaining expense?.");
-    }
-    else
-    errorMessageDisplay("Entered bill amount is less than 0. Put your spex first.");
+    if ((billAmount.value) && (cashReceived.value)) {
+        if ((billAmount.value >= 0) && (cashReceived.value >= 0)) {
+            if ((Number(billAmount.value)) != (Number(cashReceived.value))) {
+                if ((Number(cashReceived.value) >= Number(billAmount.value))) {
+                    const changeAmount = (Number(cashReceived.value) - Number(billAmount.value));
+                    errorMessageDisplay("Change to be returned is Rs."+ changeAmount);
+                    remainingAmount(changeAmount);
+                } else
+                    {errorMessageDisplay("We have an opening for a dishwashing/cleaning man. Do you wish to apply to cover the remaining expense?.");
+                    remainingAmount(0);}
+            } else
+                errorMessageDisplay("No change to be returned");
+        } else
+            errorMessageDisplay("Entered amount is less than 0.");
+    } else
+        errorMessageDisplay("Enter value in both the fields!");
 });
 
-function remainingAmount(amount)
-{
-    for(let i =0; i<availableNotes.length;i++)
-    {
-    const Notes = Math.trunc(amount/availableNotes[i]);
-    amount= amount%availableNotes[i];
-    noOfNotes[i].innerText = Notes;
+function remainingAmount(amount) {
+    for (let i = 0; i < availableNotes.length; i++) {
+        const Notes = Math.trunc(amount / availableNotes[i]);
+        amount = amount % availableNotes[i];
+        noOfNotes[i].innerText = Notes;
     }
 }
 
-function errorMessageDisplay(msg)
-{
-    message.style.display="block";
-    message.innerText = "Message: "+msg;
+function errorMessageDisplay(msg) {
+    message.style.display = "block";
+    message.innerText = "Message: " + msg;
 }
 
-function hideMessage()
-{
+function hideMessage() {
     message.style.display = "none";
+    //noOfNotes.style.display = "none";
 }
